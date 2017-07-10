29f03893568cd1132b9c63e250e51025cff2940a
coinmaster <coinmast@gmail.com>
2017-07-10 22:22:51 +0000

    ariescoin-master
    ariescoin-master

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ariescoin-qt message queue name
+#define BITCOINURI_QUEUE_NAME "ariescoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
